5885d7be99aac9b51777d572598f3288fbb6bfed
Kev-Dev <Tolkacev@outlook.de>
2020-08-08 11:59:12 +0200

    Update kompiler-kernel.sh

--- kompiler-kernel.sh
+++ kompiler-kernel.sh
@@ -3,7 +3,6 @@
 # Requirements for Debian/Ubuntu
 echo "Install requirements on Debian/Ubuntu"
 echo ""
-echo ""
 sudo apt-get install git fakeroot build-essential ncurses-dev xz-utils libssl-dev bc flex libelf-dev bison
 
 # Create a Kernel Folder
@@ -14,10 +13,9 @@ cd /$HOME/Kernel/
 
 # Downloading the Kernel-source over git
 git clone https://github.com/torvalds/linux.git
-
 cd /linux/
 
-# Copy the last insalled Kernel-source
+# Copy the last installed Kernel-source
 cp /boot/config-$(uname -r) .config
 
 # Make with Menuconfig (Go to Exit and hit enter)
