625f9656e475532609d42a60f5e5b324d16604b9
Kev-Dev <kev-dev1@users.noreply.github.com>
2021-06-30 23:13:42 +0200

    Update the compiler
    The requirement is remove for Debian/Ubuntu systems. You must install the requrements manualy.

--- compiler-kernel.sh
+++ compiler-kernel.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
-# Requirements for Debian/Ubuntu
-echo "Install requirements on Debian/Ubuntu"
-echo ""
-sudo apt-get install git fakeroot build-essential ncurses-dev xz-utils libssl-dev bc flex libelf-dev bison -y
-clear
-
 # Create a Kernel Folder
+echo "Create the Kernel folder"
 mkdir ~/Kernel/
 
 # Go to the Folder
 cd ~/Kernel/
 
 # Downloading the Kernel-source over git
-echo "Downloading Last Linux Kernel"
+echo "Downloading Last Linux Kernel over git"
 echo ""
 git clone https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git
 cd ~/Kernel/linux/
 
 # Copy the last installed Kernel-source
+echo "Fetch the actually configuration"
 cp /boot/config-$(uname -r) .config
 
 # Make with Menuconfig (Go to Exit and hit enter)
 make menuconfig
 
 # Kernel compiler
+echo "Build the new Kernel"
 make && make modules_install && sudo make install
+
+echo "Congratulation, your Linux Kernel is finish"
+echo "You use the new Linux Kernel on your System"
